c8ff7671deb69928d845214e149960c9fa1e784c
Abdusamatov00 <furqatisoqov288@gmail.com>
2025-09-14 11:33:57 +0500

    I wrote index.ts

---
+++ constants/index.ts
@@ -0,0 +1,46 @@
+import { Clapperboard, Home, Layers2, TvMinimalPlay } from "lucide-react";
+
+export const navigation_items = [
+    { title: "Home", route: "/", icon: Home},
+    { title: "Lives", route: "/lives", icon: Layers2},
+    { title: "Subscription", route: "/subscription", icon: TvMinimalPlay},
+    { title: "Dashboard", route: "/dashboard", icon: Clapperboard},
+]
+
+export const following_items = [
+    { id: 1, username: "Jahongir", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 982,},
+    { id: 2, username: "Dilshod", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 2095,},
+    { id: 3, username: "Xondamir", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 122,},
+    { id: 4, username: "Alisheyx", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 169,},
+    { id: 5, username: "Ibrohim", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 82,},
+    { id: 6, username: "Sulton", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 260,},
+    { id: 7, username: "Ahmad", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 36,},
+    { id: 8, username: "Mahmud", avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13", followingBy: 2808,},
+]
+
+export const recomended_items = [
+  {
+    id: 1,
+    username: "joni",
+    avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13",
+    followingBy: 25,
+  },
+  {
+    id: 2,
+    username: "Dilshod",
+    avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13",
+    followingBy: 30,
+  },
+  {
+    id: 3,
+    username: "alyosh",
+    avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13",
+    followingBy: 12,
+  },
+  {
+    id: 4,
+    username: "shamsKing",
+    avatar: "https://avatars.mds.yandex.net/i?id=2c07963082dbc60c0ae6b4eeeb69e191afac9d3c-5100139-images-thumbs&n=13",
+    followingBy: 50,
+  },
+];
